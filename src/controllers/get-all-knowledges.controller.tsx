9a9c7fcb9060e71dc947b9a447a75b74dbf5d602
ガモ <natsuno.gamo@ga.aais-lab.org>
2026-08-15 16:09:20 +0900

    ナレッジの永続化

--- src/controllers/get-all-knowledges.controller.tsx
+++ src/controllers/get-all-knowledges.controller.tsx
@@ -2,7 +2,7 @@ import { KnowledgeListFeature } from '../features/KnowledgeListFeature.js';
 import { KnowledgeRepository } from '../models/knowledge.repository.js';
 
 export async function getAllKnowledgesController(userId: string) {
-  const knowledges = await KnowledgeRepository.getAll();
+  const knowledges = await KnowledgeRepository.getAll_api();
 
   return <KnowledgeListFeature knowledges={knowledges} userId={userId} />;
 }
